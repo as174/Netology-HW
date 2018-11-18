8d256aa2b74ccef962f28faa91ef5ccc6782aad4
as174 <42006639+as174@users.noreply.github.com>
2018-11-18 23:13:09 +0300

    Update HW classes.py

--- HW classes.py
+++ HW classes.py
@@ -35,11 +35,15 @@ class animals():
 
 #grey and white
 class goose(animals):
+    
+    goose_list =[]
+    
     def __init__(self, name, weight):
         super().__init__(name, weight)
         self.voice = 'ga-ga-ga'
         self.eggs = 5 #qty
         self.animal_type = 'goose'
+        goose.goose_list.append(self)
         
     def pick_eggs(self, value):
         if value > self.eggs:
@@ -51,11 +55,15 @@ class goose(animals):
 
 #Manya
 class cow(animals):
+    
+    cow_list =[]
+    
     def __init__(self, name, weight):
         super().__init__(name, weight)
         self.voice = 'mooooo'
         self.milk = 1.500 #l
         self.animal_type = 'cow'
+        cow.cow_list.append(self)
         
     def collect_milk(self, value):
         if value > self.milk:
@@ -66,11 +74,15 @@ class cow(animals):
 
 #Barashek and kudryaviy
 class sheep(animals):
+    
+    sheep_list = []
+    
     def __init__(self, name, weight):
         super().__init__(name, weight)
         self.voice = 'beeeee'
         self.wool = 1.5 #kg
         self.animal_type = 'sheep'
+        sheep.sheep_list.append(self)
         
     def shear(self, value):
         if value > self.wool:
@@ -81,11 +93,15 @@ class sheep(animals):
 
 #co-co and kukareku
 class chicken(animals):
+    
+    chicken_list = []
+    
     def __init__(self, name, weight):
         super().__init__(name, weight)
         self.voice = 'ko-ko-ko'
         self.eggs = 3 #qty
         self.animal_type = 'chicken'
+        chicken.chicken_list.append(self)
         
     def pick_eggs(self, value):
         if value > self.eggs:
@@ -97,11 +113,15 @@ class chicken(animals):
         
 #horns and hooves
 class goat(animals):
+    
+    goat_list = []
+    
     def __init__(self, name, weight):
         super().__init__(name, weight)
         self.voice = 'meeeee'
         self.milk = 0.5 #l
         self.animal_type = 'goat'
+        goat.goat_list.append(self)
         
     def collect_milk(self, value):
         if value > self.milk:
@@ -112,11 +132,15 @@ class goat(animals):
         
 #kryakva
 class duck(animals):
+    
+    duck_list = []
+    
     def __init__(self, name, weight):
         super().__init__(name, weight)
         self.voice = 'krya-krya-krya'
         self.eggs = 2 #qty
         self.animal_type = 'duck'
+        duck.duck_list.append(self)
         
     def pick_eggs(self, value):
         if value > self.eggs:
@@ -149,12 +173,33 @@ duck_kryakva = duck('Kryakva', 2)
 for animals in animals.animals_list:
     animals.feed(1)
 
+print('\n')
+
+for goose in goose.goose_list:
+    goose.pick_eggs(5)
+    
+for cow in cow.cow_list:
+    cow.collect_milk(1)
+
+for sheep in sheep.sheep_list:
+    sheep.shear(1)
+    
+for chicken in chicken.chicken_list:
+    chicken.pick_eggs(2)
+    
+for goat in goat.goat_list:
+    goat.collect_milk(0.5)
+
+for duck in duck.duck_list:
+    duck.pick_eggs(2)
+    
+print('\n')
 
 weight = 0
 
 for animals in animals.animals_list:
     weight += animals.weight
-print('Вес всех животных: ', weight, 'кг')
+print('\n', 'Вес всех животных: ', weight, 'кг')
 
 
     

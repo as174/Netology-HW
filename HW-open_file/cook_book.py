3fd2dd5c3317ca68e08cab84670b659691309f2d
as174 <42006639+as174@users.noreply.github.com>
2018-11-24 01:54:35 +0300

    Update cook_book.py

--- HW-open_file/cook_book.py
+++ HW-open_file/cook_book.py
@@ -22,13 +22,91 @@ with open('cook_book.txt') as f:
     pass
 pass 
 
-for k in cook_book.keys():
-    print(k, '\n', cook_book[k], '\n')
-pass
+#создаем пустой словарь shop_list
+#передаем в функцию параметры - ключи и количество персон
+#получаем название блюда - ключ
+#берем значение ключа - список
+#делаем цикл по списку
+#в списке находим ingredients_name и его значение используем в качестве ключа для shop_list
 
 
-#            ingredients_dict = {}
-#            ingredients_dict['ingredients_name'] = kuku[0].strip()
-#            ingredients_dict['qty'] = kuku[1].strip()
-#            ingredients_dict['measure'] = kuku[2].strip()
-                
+
+#shop_list = {}
+#msr_qty_list = []
+#
+#a = cook_book['Омлет'][0]['ingredients_name'] 
+#msr = cook_book['Омлет'][0]['measure'] 
+#qty = cook_book['Омлет'][0]['qty'] 
+#shop_list[a] = msr_qty
+
+
+
+#def shop_list(dishes):
+#    ingr_list = []
+#    for dish in dishes:
+#        if dish in cook_book.keys(): #проверить является ли он ключом в словаре cook_book и вернуть значение
+#            print(cook_book[dish], '\n')
+#        pass
+#    pass
+
+def shop_list(dishes):
+    shop_dict = {}
+    ingr_list = []
+    for dish in dishes:
+        if dish in cook_book.keys(): #проверить является ли он ключом в словаре cook_book и вернуть значение
+            for i in range(len(cook_book[dish])):
+                ingr_list.append(cook_book[dish][i])
+            pass
+    print(ingr_list)
+    key_ingr_list = ['measure', 'quantity']
+    for i in range(len(ingr_list)):
+        for k in i:
+            qty_msr = [ingr_list['measure'], ingr_list['qty']]
+            ingr_name = ingr_list['ingredients_name']
+            temp_dict = dict(zip(key_ingr_list, qty_msr))
+            temp_dict2 = {ingr_name : temp_dict}
+            shop_dict.append(temp_dict2)
+        pass
+    pass
+    print(shop_dict)
+            
+shop_list(['Омлет', 'Запеченный картофель'])
+
+#пройти итератором по списку словарей, взять значение ключа "ingredients name", 
+#сложить значение ключа "qty" и вывести значение из "measure"
+            
+
+#
+#def shop_list(*args):
+#    for k in cook_book.keys():
+#        if 
+
+#def get_shop_list_by_dishes(dishes):
+#    shop_list = {}
+#    for dishes in cook_book.keys():
+#        shop_list = (cook_book[dishes]).append
+#        print(shop_list.values())
+#    pass
+#
+#get_shop_list_by_dishes(['Омлет', 'Фахитос'])
+
+#shop_list = {}
+#
+#for v in cook_book.values():
+    
+    
+
+#Нужно написать функцию, которая на вход принимает список блюд из cook_book и количество персон
+#get_shop_list_by_dishes(dishes, person_count)
+#На выходе мы должны получить словарь с названием ингредиентов и его количетсва для блюда. 
+#get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+#Должен быть следующий результат:
+#
+#{
+#  'Картофель': {'measure': 'кг', 'quantity': 2},
+#  'Молоко': {'measure': 'мл', 'quantity': 200},
+#  'Помидор': {'measure': 'шт', 'quantity': 8},
+#  'Сыр гауда': {'measure': 'г', 'quantity': 200},
+#  'Яйцо': {'measure': 'шт', 'quantity': 4},
+#  'Чеснок': {'measure': 'зубч', 'quantity': 6}
+#}

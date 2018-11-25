8d29ef690b1e1a4682c9c19d97790596adfa82f6
as174 <42006639+as174@users.noreply.github.com>
2018-11-25 19:46:28 +0300

    Update HW classes.py

--- HW classes.py
+++ HW classes.py
@@ -6,7 +6,7 @@ Created on Sun Nov 18 12:37:28 2018
 """
 
 #parent class
-class animal():
+class Animal():
     
     hungry = True
     animals_list = []
@@ -14,7 +14,7 @@ class animal():
     def __init__(self, name, weight):
         self.name = name
         self.weight = weight
-        animal.animals_list.append(self)
+        Animal.animals_list.append(self)
     
 #    def name(self, value):
 #        self.name = value
@@ -34,7 +34,7 @@ class animal():
 #дочерние классы
 
 #grey and white
-class goose(animal):
+class Goose(Animal):
     
     goose_list =[]
     
@@ -43,7 +43,7 @@ class goose(animal):
         self.voice = 'ga-ga-ga'
         self.eggs = 5 #qty
         self.animal_type = 'goose'
-        goose.goose_list.append(self)
+        Goose.goose_list.append(self)
         
     def pick_eggs(self, value):
         if value > self.eggs:
@@ -54,7 +54,7 @@ class goose(animal):
             
 
 #Manya
-class cow(animal):
+class Cow(Animal):
     
     cow_list =[]
     
@@ -63,7 +63,7 @@ class cow(animal):
         self.voice = 'mooooo'
         self.milk = 1.500 #l
         self.animal_type = 'cow'
-        cow.cow_list.append(self)
+        Cow.cow_list.append(self)
         
     def collect_milk(self, value):
         if value > self.milk:
@@ -73,7 +73,7 @@ class cow(animal):
             print("You've collected", value, "milk from", self.name)
 
 #Barashek and kudryaviy
-class sheep(animal):
+class Sheep(Animal):
     
     sheep_list = []
     
@@ -82,7 +82,7 @@ class sheep(animal):
         self.voice = 'beeeee'
         self.wool = 1.5 #kg
         self.animal_type = 'sheep'
-        sheep.sheep_list.append(self)
+        Sheep.sheep_list.append(self)
         
     def shear(self, value):
         if value > self.wool:
@@ -92,7 +92,7 @@ class sheep(animal):
             print("You've sheared", value, "wool from", self.name)
 
 #co-co and kukareku
-class chicken(animal):
+class Chicken(Animal):
     
     chicken_list = []
     
@@ -101,7 +101,7 @@ class chicken(animal):
         self.voice = 'ko-ko-ko'
         self.eggs = 3 #qty
         self.animal_type = 'chicken'
-        chicken.chicken_list.append(self)
+        Chicken.chicken_list.append(self)
         
     def pick_eggs(self, value):
         if value > self.eggs:
@@ -112,7 +112,7 @@ class chicken(animal):
         
         
 #horns and hooves
-class goat(animal):
+class Goat(Animal):
     
     goat_list = []
     
@@ -121,7 +121,7 @@ class goat(animal):
         self.voice = 'meeeee'
         self.milk = 0.5 #l
         self.animal_type = 'goat'
-        goat.goat_list.append(self)
+        Goat.goat_list.append(self)
         
     def collect_milk(self, value):
         if value > self.milk:
@@ -131,7 +131,7 @@ class goat(animal):
             print("You've collected", value, "milk from", self.name)
         
 #kryakva
-class duck(animal):
+class Duck(Animal):
     
     duck_list = []
     
@@ -140,7 +140,7 @@ class duck(animal):
         self.voice = 'krya-krya-krya'
         self.eggs = 2 #qty
         self.animal_type = 'duck'
-        duck.duck_list.append(self)
+        Duck.duck_list.append(self)
         
     def pick_eggs(self, value):
         if value > self.eggs:
@@ -150,55 +150,55 @@ class duck(animal):
             print("You've picked", value, "eggs from", self.name)
         
 
-goose_grey = goose('Grey', 2)
-goose_white = goose('White', 3)
+goose_grey = Goose('Grey', 2)
+goose_white = Goose('White', 3)
 
-cow_manya = cow('Manya', 150)
+cow_manya = Cow('Manya', 150)
 
-sheep_barashek = sheep('Barashek', 40)
-sheep_kudryaviy = sheep('Kudryaviy', 45)
+sheep_barashek = Sheep('Barashek', 40)
+sheep_kudryaviy = Sheep('Kudryaviy', 45)
 
-chicken_koko = chicken('Koko', 2)
-chicken_kukareku = chicken('Kukareku', 1)
+chicken_koko = Chicken('Koko', 2)
+chicken_kukareku = Chicken('Kukareku', 1)
 
-goat_horn = goat('Horn', 15)
-goat_hoove = goat('Hoove', 17)
+goat_horn = Goat('Horn', 15)
+goat_hoove = Goat('Hoove', 17)
 
-duck_kryakva = duck('Kryakva', 2)
+duck_kryakva = Duck('Kryakva', 2)
 
 
 #chicken_koko.feed(2)
 #animals.feed(2)
 
-for animal in animal.animals_list:
-    animal.feed(1)
+for Animal in Animal.animals_list:
+    Animal.feed(1)
 
 print('\n')
 
-for goose in goose.goose_list:
-    goose.pick_eggs(5)
+for Goose in Goose.goose_list:
+    Goose.pick_eggs(5)
     
-for cow in cow.cow_list:
-    cow.collect_milk(1)
+for Cow in Cow.cow_list:
+    Cow.collect_milk(1)
 
-for sheep in sheep.sheep_list:
-    sheep.shear(1)
+for Sheep in Sheep.sheep_list:
+    Sheep.shear(1)
     
-for chicken in chicken.chicken_list:
-    chicken.pick_eggs(2)
+for Chicken in Chicken.chicken_list:
+    Chicken.pick_eggs(2)
     
-for goat in goat.goat_list:
-    goat.collect_milk(0.5)
+for Goat in Goat.goat_list:
+    Goat.collect_milk(0.5)
 
-for duck in duck.duck_list:
-    duck.pick_eggs(2)
+for Duck in Duck.duck_list:
+    Duck.pick_eggs(2)
     
 print('\n')
 
 weight = 0
 
-for animal in animal.animals_list:
-    weight += animal.weight
+for Animal in Animal.animals_list:
+    weight += Animal.weight
 print('Вес всех животных: ', weight, 'кг')
 
 all_animals = [goose_grey.__dict__, goose_white.__dict__, cow_manya.__dict__, sheep_barashek.__dict__, sheep_kudryaviy.__dict__, chicken_koko.__dict__, chicken_kukareku.__dict__, goat_horn.__dict__, goat_hoove.__dict__, duck_kryakva.__dict__]

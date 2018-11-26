422f24317c8e2b1ea6eaea87320348827ef864b9
as174 <42006639+as174@users.noreply.github.com>
2018-11-26 19:21:25 +0300

    Create HW-except3.py

---
+++ HW-except/HW-except3.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+try:
+    polskaya_notacia = input('Введите аргументы польской нотации: ')
+    arguments_list = polskaya_notacia.split()
+
+    arguments_list[1] = int(arguments_list[1])
+    arguments_list[2] = int(arguments_list[2])
+
+    assert arguments_list[1] >= 0 
+    assert arguments_list[2] >= 0
+    
+    if arguments_list[0] == '+':
+        print(arguments_list[1] + arguments_list[2])
+    elif arguments_list[0] == '-':
+        print(arguments_list[1] - arguments_list[2])
+    elif arguments_list[0] == '*':
+        print(arguments_list[1] * arguments_list[2])
+    elif arguments_list[0] == '/':
+        try:
+            print(arguments_list[1] / arguments_list[2])
+        except ZeroDivisionError: 
+            print('На ноль делить нельзя')
+    else:
+        print('Оператор не найден. Попробуйте еще раз')
+    
+except:
+    print('Введите положительные числа')
+
+
+#if arguments_list[0] == '+':
+#    print(arguments_list[1] + arguments_list[2])
+#elif arguments_list[0] == '-':
+#    print(arguments_list[1] - arguments_list[2])
+#elif arguments_list[0] == '*':
+#    print(arguments_list[1] * arguments_list[2])
+#elif arguments_list[0] == '/':
+#    try:
+#        print(arguments_list[1] / arguments_list[2])
+#    except ZeroDivisionError: 
+#        print('На ноль делить нельзя')
+#else:
+#    print('Оператор не найден. Попробуйте еще раз')

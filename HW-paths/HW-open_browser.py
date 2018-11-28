f747ac35015b61f42e0039a91ce0ef9d2e59635b
as174 <42006639+as174@users.noreply.github.com>
2018-11-28 17:11:02 +0300

    Create HW-open_browser.py

---
+++ HW-paths/HW-open_browser.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 28 17:06:23 2018
+
+@author: RU20009341
+"""
+
+import subprocess
+
+browser = '"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"'
+
+run_browser = subprocess.Popen(browser)

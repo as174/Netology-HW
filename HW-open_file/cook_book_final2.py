17d4f960e6b57aec7eae1b7f8d1856905d1456f3
as174 <42006639+as174@users.noreply.github.com>
2018-12-06 20:07:25 +0300

    Create cook_book_final2.py

---
+++ HW-open_file/cook_book_final2.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: AS
+"""
+
+def shop_list(dishes, person = 1):
+    cook_book = {}
+    with open('cook_book.txt') as f:
+        for line in f:
+            dish = line.strip() #1 строка - название блюда
+            qty = f.readline().strip() #количество ингредиентов
+            ingredients_list = [] 
+            for i in range(int(qty)): #цикл по ингредиентам
+                ingredient = f.readline().strip().split(' | ') 
+                ingredients_list.append(dict(zip(['ingredients_name', 'qty', 'measure'], ingredient)))
+            f.readline() #прочитать разделитель
+            cook_book[dish] = ingredients_list
+    shop_dict = {}
+    ingr_list = []
+    for dish in dishes:
+        if dish in cook_book.keys(): #проверить является ли он ключом в словаре cook_book и вернуть значение
+            for i in range(len(cook_book[dish])):
+                ingr_list.append(cook_book[dish][i])
+    key_ingr_list = ['measure', 'quantity']
+    for i in range(len(ingr_list)):
+        qty_msr = [ingr_list[i]['measure'], int(ingr_list[i]['qty'])*person]
+        ingr_name = ingr_list[i]['ingredients_name']
+        qty_msr_dict = dict(zip(key_ingr_list, qty_msr))
+        ingr_qty_msr_dict = {ingr_name : qty_msr_dict}
+        shop_dict.update(ingr_qty_msr_dict)
+    print(shop_dict)
+            
+shop_list(['Омлет', 'Запеченный картофель'], 5)
